bd915e02ed122449fa905db08ba2086e3c01fce5
Hitesh Garg <garg95htesh@gmail.com>
2016-09-27 23:07:36 +0530

    Models, Templates and Views

--- Rango/models.py
+++ Rango/models.py
@@ -1,9 +1,19 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+
+"""
+"how do i create a slug in django" turns into "how-do-i-create-a-slug-in-django"
+"""
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes =  models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = 'categories'
--- populate_rango.py
+++ populate_rango.py
@@ -10,25 +10,33 @@ from Rango.models import Category, Page
 def populate():
     python_pages = [
         {"title": "Official Python Tutorial",
-        "url":"http://docs.python.org/2/tutorial/"},
+        "url":"http://docs.python.org/2/tutorial/",
+        "views":100},
         {"title":"How to Think like a Computer Scientist",
-        "url":"http://www.greenteapress.com/thinkpython/"},
+        "url":"http://www.greenteapress.com/thinkpython/",
+        "views":50},
         {"title":"Learn Python in 10 Minutes",
-        "url":"http://www.korokithakis.net/tutorials/python/"} ]
+        "url":"http://www.korokithakis.net/tutorials/python/",
+        "views":25} ]
 
     django_pages = [
         {"title":"Official Django Tutorial",
-        "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+        "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/",
+        "views":200},
         {"title":"Django Rocks",
-        "url":"http://www.djangorocks.com/"},
+        "url":"http://www.djangorocks.com/",
+        "views":100},
         {"title":"How to Tango with Django",
-        "url":"http://www.tangowithdjango.com/"} ]
+        "url":"http://www.tangowithdjango.com/",
+        "views":50} ]
 
     other_pages = [
         {"title":"Bottle",
-        "url":"http://bottlepy.org/docs/dev/"},
+        "url":"http://bottlepy.org/docs/dev/",
+        "views":400},
         {"title":"Flask",
-        "url":"http://flask.pocoo.org"} ]
+        "url":"http://flask.pocoo.org",
+        "views":200} ]
 
     cats = {"Python": {"pages": python_pages,
                         "views":128,
