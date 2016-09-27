cf764e62c0b855751d878d2ae5230e466e24bb64
Hitesh Garg <garg95htesh@gmail.com>
2016-09-27 22:03:42 +0530

    Templates and Static Media

--- Rango/views.py
+++ Rango/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hi")
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'Rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page")
+    return render(request, 'Rango/about.html')
--- RangoApp/settings.py
+++ RangoApp/settings.py
@@ -53,10 +53,12 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'RangoApp.urls'
 
+
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,5 +120,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 STATIC_URL = '/static/'
