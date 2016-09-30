52fe033c803da870ab8bfba61da85c49392da6c4
Hitesh Garg <garg95htesh@gmail.com>
2016-09-30 19:47:59 +0530

    basic configurations for heroku

--- RangoApp/settings.py
+++ RangoApp/settings.py
@@ -146,10 +146,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR, ]
 
